35638b6ff74ab97a81b6d828f1682e101af833b3
Annuh Liwan <annuhliwan@gmail.com>
2026-02-26 03:23:08 +0700

    fix deploy

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -38,7 +38,7 @@ export const ThreeScene = ({ onSceneReady }: ThreeSceneProps) => {
     renderer.setSize(containerRef.current.clientWidth, containerRef.current.clientHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFShadowShadowMap;
+    renderer.shadowMap.type = THREE.PCFShadowMap;
     containerRef.current.appendChild(renderer.domElement);
     rendererRef.current = renderer;
 
